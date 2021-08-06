3a34e6f272fcd0944770542e0d61585cab9fb419
Alabs02 <alabson.inc@gmail.com>
2021-08-06 01:09:23 +0100

    Initail commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Lets Build a LinkedIn CLONE!</h1>
+    </div>
+  );
+}
+
+export default App;
